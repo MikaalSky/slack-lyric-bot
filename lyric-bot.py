9cf477ea0b1c66f31c2017237b666bdc4eb9171d
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-11-10 09:13:48 -0600

    Actually fix the indentation

--- lyric-bot.py
+++ lyric-bot.py
@@ -1,54 +1,47 @@
 import random
 import BaseHTTPServer
 
-# Song lyrics are (sometimes) inspirational...
+HOSTNAME = "localhost"
+PORTNUMB = 5555
 
+# TODO (Gigabyte Giant): Move this to a JSON file
 lyrics = [
-	{
-		"lyric": "If you choose not to decide, you still have made a choice",
-		"band": "Rush",
-		"song": "Freewill"
-	},
-	{
-		"lyric": "All that you can do is wish them well",
-		"band": "Rush",
-		"song": "Wish them well"
-	},
-	{
-		"lyric": "The measure of a life is a measure of love and respect, so hard to earn so easily burned",
-		"band": "Rush",
-		"song": "The Garden"
-	}
+    {
+        "lyric": "If you choose not to decide, you still have made a choice",
+        "band": "Rush",
+        "song": "Freewill"
+    },
+    {
+        "lyric": "All that you can do is wish them well",
+        "band": "Rush",
+        "song": "Wish them well"
+    },
+    {
+        "lyric": "The measure of a life is a measure of love and respect, so hard to earn, so easily burned",
+        "band": "Rush",
+        "song": "The Garden"
+    }
 ]
 
-def formatStr(writeStr):
-	"{\"response_type\":\"in_channel\",\"text\":\"" + writeStr + "\"}"
+def doJSONResponse(responseText):
+    return "{\"response_type\":\"in_channel\",\"text\":\"" + responseText + "\"}"
 
-def pickSongLogic():
-	pickedSong = lyrics[random.randrange(len(lyrics))]
-	writeStr = "_" + pickedSong["lyric"] + "_ -- *" + pickedSong["band"] + ", " + pickedSong["song"] + "*"
+def pickRandomLyric():
+    pickedSong = lyrics[random.randrange(len(lyrics))]
+    return "_" + pickedSong["lyric"] + "_ -- *" + pickedSong["band"] + ", " + pickedSong["song"] + "*"
 
-	return writeStr
+class lyricBotHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    # TODO (Gigabyte Giant): Support POST requests
 
-class gigaHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-	# def do_POST(self):
-	# 	print("POST")
-	# 	self.send_response(200)
-	# 	self.send_header("Content-type", "application/json")
-	# 	self.end_headers()
-		
-	# 	data = self.rfile.read()
- 	#   cmdTxt = str(str(data).split("&text=")[1]).split("&")[0])
- 	#   print(cmdTxt)
-	# 	self.wfile.write(formatStr("@GigabyteGiant will implement this soon enough..."))
+    def do_GET(self):
+        print("GET request")
 
-	def do_GET(self):
-		print(self.request)
-		self.send_response(200)
-		self.send_header("Content-type", "application/json")
-		self.end_headers()
+        self.send_response(200)
+        self.send_headers("Content-Type", "application/json")
+        self.end_headers()
 
-		self.wfile.write(formatStr(pickSongLogic()))
+        self.wfile.write(doJSONResponse(pickRandomLyric()))
 
-httpd = BaseHTTPServer.HTTPServer(("107.191.102.11", 5555), gigaHTTPRequestHandler)
-httpd.serve_forever()
+httpServer = BaseHTTPServer.HTTPServer((HOSTNAME, PORTNUMB), lyricBotHTTPRequestHandler)
+print("HTTP Server started at " + HOSTNAME + ":" + str(PORTNUMB))
+httpServer.serve_forever()
