929492026cce6943c4f26ad371df360ca293b674
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-11-09 19:45:36 -0600

    Prepare to handle POST requests

--- lyric-bot.py
+++ lyric-bot.py
@@ -21,18 +21,29 @@ lyrics = [
 	}
 ]
 
+def pickSongLogic():
+	pickedSong = lyrics[random.randrange(len(lyrics))]
+	writeStr = "_" + pickedSong["lyric"] + "_ -- *" + pickedSong["band"] + ", " + pickedSong["song"] + "*"
+
+	return "{\"response_type\":\"in_channel\",\"text\":\"" + writeStr + "\"}"
+
 class gigaHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-	def do_GET(self):
-	print(self.request)
+	def do_POST(self):
+		print("POST")
+		print(self.request)
 		self.send_response(200)
 		self.send_header("Content-type", "application/json")
 		self.end_headers()
 
-		pickedSong = lyrics[random.randrange(len(lyrics))]
+		self.wfile.write(pickSongLogic)
 
-		writeStr = "_" + pickedSong["lyric"] + "_ -- *" + pickedSong["band"] + ", " + pickedSong["song"] + "*"
+	def do_GET(self):
+		print(self.request)
+		self.send_response(200)
+		self.send_header("Content-type", "application/json")
+		self.end_headers()
 
-		self.wfile.write("{\"response_type\":\"in_channel\",\"text\":\"" + writeStr + "\"}")
+		self.wfile.write(pickSongLogic)
 
 httpd = BaseHTTPServer.HTTPServer(("107.191.102.11", 5555), gigaHTTPRequestHandler)
 httpd.serve_forever()
