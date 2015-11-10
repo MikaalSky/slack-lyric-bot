ed74eb195c4a3577e332e21a84138a4c1983154b
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-11-10 09:48:10 -0600

    Implement a really bad system for getting the command text from the POST request

--- lyric-bot.py
+++ lyric-bot.py
@@ -15,8 +15,28 @@ def pickRandomLyric():
     pickedSong = lyrics[random.randrange(len(lyrics))]
     return str(pickedSong)
 
+def addSongStr(songStr):
+    pass
+
 class lyricBotHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     # TODO (Gigabyte Giant): Support POST requests
+    def do_POST(self):
+        print("POST request")
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+
+        requestFileTxt = ""
+
+        for ln in self.rfile:
+            requestFileTxt += ln
+
+        cmdTxt = requestFileTxt.split("&text=")[1].split("&")[0]
+
+        if (cmdTxt.split(" ")[0] === "add") {
+            print("They want to add a song!")
+        }
 
     def do_GET(self):
         print("GET request")
